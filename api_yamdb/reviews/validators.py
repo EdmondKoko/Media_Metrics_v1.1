23ad35cf874a979c427f665b5f9739fcc2fb4edb
EdmondKoko <107623627+EdmondKoko@users.noreply.github.com>
2023-10-18 12:19:14 +0300

    Update validators.py

--- api_yamdb/reviews/validators.py
+++ api_yamdb/reviews/validators.py
@@ -4,7 +4,6 @@ from django.utils import timezone
 
 def year_validator(value):
     """Валидатор для определения года."""
-
     if not (0 < value <= timezone.now().year):
         raise ValidationError(
             '%(value)s год указан не корректно',
