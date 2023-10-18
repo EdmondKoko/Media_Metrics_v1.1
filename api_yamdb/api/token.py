094ed4e06337eede2a63cc65efcd2672100b9300
EdmondKoko <107623627+EdmondKoko@users.noreply.github.com>
2023-10-18 12:10:24 +0300

    Update token.py

--- api_yamdb/api/token.py
+++ api_yamdb/api/token.py
@@ -5,7 +5,6 @@ from api_yamdb.settings import EMAIL
 
 def send_code(email, confirmation_code):
     """Oтправляем код на почту пользователя."""
-
     send_mail(
         subject='Код подтверждения',
         message=f'Ваш код подтверждения: {confirmation_code}',
