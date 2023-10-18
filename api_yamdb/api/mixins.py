2a66e4b970c0c3b5ca1c86fbd86375656176bfac
EdmondKoko <107623627+EdmondKoko@users.noreply.github.com>
2023-10-18 11:45:03 +0300

    Update mixins.py

--- api_yamdb/api/mixins.py
+++ api_yamdb/api/mixins.py
@@ -1,6 +1,6 @@
 from rest_framework import mixins, viewsets
 
-
+"""ViewSet, который предоставляет действия по умолчанию `create()`, `destroy()` и `list()`."""
 class ListCreateDestroyViewSet(
     mixins.CreateModelMixin,
     mixins.DestroyModelMixin,
